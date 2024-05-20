1c5348c2ed54263e8c105b71194b3d1fbaa15269
lvl0_module2 <zoey.lestyk@gmail.com>
2024-05-20 16:56:21 -0700

    I did RaceCar

--- src/_02_race_car/RaceCarRunner.java
+++ src/_02_race_car/RaceCarRunner.java
@@ -7,11 +7,13 @@ package _02_race_car;
 
 
 public class RaceCarRunner {
+	
 	public static void main(String[] args) {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		
+	RaceCar RaceCar = new RaceCar("Toyota", 4);
 		// 2. Print the RaceCar's position in the race
 
 		// 3. Crash the RaceCar
